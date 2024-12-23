67df3c48814a554f92d817c2578910cfd595a836
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2024-12-23 19:30:29 +0100

    refac(simulations): Separate jobs preparation from their simulation. Add some more typehints.

--- src/simulations.py
+++ src/simulations.py
@@ -10,57 +10,68 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit_aer import AerSimulator
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime import IBMBackend
 from tqdm import tqdm
 
-from src.job import LG
+from src.job import LG, Job
 from src.utils import *
 
+from typing import List
 
-def simulate_jobs():
-    # Prepare circuits.
-    job = LG()
-    qubits = [[1, 0, 2]]
+
+def prepare_jobs() -> List[Job]:
+    jobs: List[Job] = []
+    
+    # Prepare circuits. This is the part to modify.
+    job: LG = LG()
+    qubits: List[List[int]] = [[1, 0, 2]]
 
     epp: float = 0.1
 
     job.add_test_circuits(qubits, epp)
 
-    # Prepare simulator.
-    noisy: bool = False
+    jobs.append(job)
+
+    return jobs
+
+
+def simulate_jobs(jobs: List[Job], noisy: bool = False) -> None:
     print(f"\tPreparing (noisy={noisy}) simulator...")
 
-    backend = AerSimulator()
+    simulator: AerSimulator = AerSimulator()
 
     if noisy:
         backend_str: str = "ibm_brisbane"
 
         print(f"\tTargeting {backend_str}...")
 
-        service = QiskitRuntimeService(
+        service: QiskitRuntimeService = QiskitRuntimeService(
             channel="ibm_quantum",
             token=TOKENS[TOKEN_VARIABLES[0]],
         )        
 
-        target_backend = service.get_backend(backend_str)
-        backend = backend.from_backend(target_backend)
+        backend: IBMBackend = service.get_backend(backend_str)
+        simulator = simulator.from_backend(backend)
 
     print("\tRunning the circuits...\n")
-    sampler = Sampler(backend=backend)
+    sampler = Sampler(backend=simulator)
 
-    for i, circuit in enumerate(job.circuits):
-        result = sampler.run([circuit], shots=1000).result()
-        data_pub = result[0].data 
+    for job in jobs:
+        for i, circuit in enumerate(job.circuits):
+            result = sampler.run([circuit], shots=1000).result()
+            data_pub = result[0].data 
 
-        # cr0 is the name of classical register we use
-        counts = data_pub.cr0.get_counts()
-        print(f"\t\tJob {i} counts: {counts}")
+            # cr0 is the name of classical register we use
+            counts = data_pub.cr0.get_counts()
+            print(f"\t\tJob {i} counts: {counts}")
    
 
-def main():
-    print("Start")
-    simulate_jobs()
-    print("Done")
-
+def main() -> None:
+    jobs: List[Job] = prepare_jobs()
+    simulate_jobs(jobs)
+    
 
 if __name__ == "__main__":
+    print("Start")
     main()
+    print("Done")
